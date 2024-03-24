b995da532392819741b14b6942fd05c0748de5ed
“zwx215” <zwx215@alumni.ku.dk>
2024-03-24 21:10:16 +0100

    Uploading project to git

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,15 +1,1 @@
-def square(x):
-    """ square numpy array
-    
-    Args:
-    
-        x (ndarray): input array
-        
-    Returns:
-    
-        y (ndarray): output array
-    
-    """
-    
-    y = x**2
-    return y
+#I did not use this
