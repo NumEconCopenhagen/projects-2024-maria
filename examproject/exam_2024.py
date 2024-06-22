4349524eec48f7c0af8944cafc6805159b2ceb52
“zwx215” <zwx215@alumni.ku.dk>
2024-06-23 01:13:08 +0200

    exam backup 2

--- examproject/exam_2024.py
+++ examproject/exam_2024.py
@@ -4,7 +4,7 @@ from scipy import optimize
 from types import SimpleNamespace
 
 class production_economy:
-    def __init__(self, par, tau, T):
+    def __init__(self, par):
         
         #parameters
         self.A = par.A
@@ -14,18 +14,14 @@ class production_economy:
         self.nu = par.nu
         self.epsilon = par.epsilon
 
-        self.tau = tau
-        self.T = T
+        #self.tau = tau
+        #self.T = T
 
         self.kappa = par.kappa
 
-        self.c2 = 0
-        self.p1 = 0
-        self.p2 = 0
-
-        
-        
-
+        #self.c2 = 0
+        #self.p1 = 0
+        #self.p2 = 0
 
     #firm optimal behaviour
     def opt_firm(self, pj, w=1):
@@ -38,28 +34,28 @@ class production_economy:
         return (1-self.gamma)/self.gamma * w * (pj*self.A*self.gamma/w)**(1/(1-self.gamma))
 
     #consumer optimal behaviour
-    def opt_cb(self,l, p1, p2, w=1):
-        c1 = self.alpha * (w*l+self.T+self.pi_j(p1,w)+self.pi_j(p2,w))/p1
-        c2 = (1-self.alpha) * (w*l+self.T+self.pi_j(p1,w)+self.pi_j(p2,w))/(p1+self.tau)
+    def opt_cb(self,l, p1, p2, T = 0, tau = 0 ,w=1):
+        c1 = self.alpha * (w*l+T+self.pi_j(p1,w)+self.pi_j(p2,w))/p1
+        c2 = (1-self.alpha) * (w*l+T+self.pi_j(p1,w)+self.pi_j(p2,w))/(p1+tau)
         return c1, c2
     
     #consumer optimal labour supply
     #objective function
-    def obj_ls(self, l, p1, p2):
-        c1, c2 = self.opt_cb(l,p1,p2)
+    def obj_ls(self, l, p1, p2, T, tau):
+        c1, c2 = self.opt_cb(l,p1,p2,T,tau)
         return - (np.log(c1**self.alpha * c2**(1-self.alpha)) - self.nu*l**(1+self.epsilon)/(1+self.epsilon))
 
     #solver
-    def opt_ls(self, p1, p2):
+    def opt_ls(self, p1, p2, T, tau):
         l_quess = 1
-        sol = optimize.minimize(self.obj_ls, l_quess, args=(p1, p2), method='SLSQP')
+        sol = optimize.minimize(self.obj_ls, l_quess, args=(p1, p2, T, tau), method='SLSQP')
         return sol.x[0]
     
     #EQ
     #Market clearing conditions (only 2)
-    def clear_con(self, p):
+    def clear_con(self, p, T, tau):
         error = np.zeros(2)
-        c1, c2 = self.opt_cb(self.opt_ls(p[0],p[1]), p[0], p[1])
+        c1, c2 = self.opt_cb(self.opt_ls(p[0],p[1], T, tau), p[0], p[1], T, tau)
         y1 = self.opt_firm(p[0])[1]
         y2 = self.opt_firm(p[1])[1]
         
@@ -68,51 +64,55 @@ class production_economy:
         return error
     
     #find the roots of the equation system (illustrative)
-    def EQ(self):
+    def EQ(self, T, tau):
         x0 = np.array([0.1,0.1])
-        res = optimize.root(self.clear_con, x0)
+        res = optimize.root(self.clear_con, x0, args = (T,tau))
         output =  res.x
         return output
 
-    
-    # # Opmitize consumer with varying tau (and T but constrained through error) - input = [p1,p2,]
-    # def clear_SWF_con(self, input, tau, T):
-    #     error = np.zeros(3)
-    #     p1, p2 = input
-    #     self.T = T
-    #     self.tau = tau
-    #     c1, c2 = self.opt_cb(self.opt_ls(p1,p2), p1, p2)
+    # Opmitize consumer with varying tau (and T but constrained through error) - input = [p1,p2,]
+    def clear_SWF_con(self, p, tau):
+        error = np.zeros(3)
+        c1, c2 = self.opt_cb(self.opt_ls(p[0],p[1], p[2], tau), p[0],p[1], p[2], tau)
 
-    #     y1 = self.opt_firm(p1)[1]
-    #     y2 = self.opt_firm(p2)[1]        
+        y1 = self.opt_firm(p[0])[1]
+        y2 = self.opt_firm(p[1])[1]   
 
-    #     # Define threee errors for marketclearing and constrain T
-    #     error = np.array([c1-y1, c2-y2])
-    #     return error
+        error[0] = c1-y1
+        error[1] = c2-y2
+        error[2] = p[2]-tau*c2
+        return error
 
+    # Objective function to maximize SWF
+    def SWF_obj(self, tau):
+        x0 = np.array([0.1,0.1,0])
+        res = optimize.root(self.clear_SWF_con, x0, args = (tau))
+        return res.x
 
-
-    
-    # # Objective function to maximize SWF
-    # def SWF_obj(self, tau, T):
-
-    #     self.tau = tau
-    #     self.T = T
-    #     x0 = np.array([0.1,0.1,0])
-    #     res = optimize.root(self.clear_SWF_con, x0, args = (tau,T))
-    #     p1, p2 = res.x
-    #     l = self.opt_ls(p1,p2)
-    #     c1, c2 = self.opt_cb(l, p1, p2)
-
-    #     y2 = self.opt_firm(p2)[1]
+    def SWF_objective(self, tau):
+        p1, p2, T = self.SWF_obj(tau)
+        l = self.opt_ls(p1, p2, T, tau)
+        c1, c2 = self.opt_cb(l, p1, p2, T, tau)
+        y2 = self.opt_firm(p2)[1]
         
-    #     U = np.log(c1**self.alpha * c2**(1-self.alpha)) - self.nu*l**(1+self.epsilon)/(1+self.epsilon)
+        U = np.log(c1**self.alpha * c2**(1-self.alpha)) - self.nu*l**(1+self.epsilon)/(1+self.epsilon)
+        SWF = U-self.kappa*y2
 
-    #     SWF = U-self.kappa*y2
+        return -SWF
 
-    #     return -SWF
-    
+#   # Objective function to maximize SWF
+#     def SWF_obj(self, tau, T):
+#         x0 = np.array([0.1,0.1,0])
+#         res = optimize.root(self.clear_SWF_con, x0, args = (T,tau))
+#         p1, p2 = res.x
+#         l = self.opt_ls(p1,p2)
+#         c1, c2 = self.opt_cb(l, p1, p2)
+#         y2 = self.opt_firm(p2)[1]
+        
+#         U = np.log(c1**self.alpha * c2**(1-self.alpha)) - self.nu*l**(1+self.epsilon)/(1+self.epsilon)
+#         SWF = U-self.kappa*y2
 
+#         return -SWF
 
 
     # #     p1, p2, T = self.EQ_SWF(tau)
@@ -120,43 +120,43 @@ class production_economy:
     # #     U = np.log(c1**self.alpha * c2**(1-self.alpha)) - self.nu*l**(1+self.epsilon)/(1+self.epsilon)
 
 
-    def SWF_obj(self, input):
-        # Deinfe new econ
-        self.tau, self.T = input
-        # Compute market clearing p1, p2, l, c1, c2, y2 given tau and T
-        p1, p2 = self.EQ()
-        self.p1 = p1
-        self.p2 = p2
-        l = self.opt_ls(p1,p2)
-        c1, c2 = self.opt_cb(l, p1, p2)
-        self.c2 = c2
-        y2 = self.opt_firm(p1)[1]
-        # Compute U
-        U = np.log(c1**self.alpha * c2**(1-self.alpha)) - self.nu*l**(1+self.epsilon)/(1+self.epsilon)
+    # def SWF_obj(self, input):
+    #     # Deinfe new econ
+    #     self.tau, self.T = input
+    #     # Compute market clearing p1, p2, l, c1, c2, y2 given tau and T
+    #     p1, p2 = self.EQ()
+    #     self.p1 = p1
+    #     self.p2 = p2
+    #     l = self.opt_ls(p1,p2)
+    #     c1, c2 = self.opt_cb(l, p1, p2)
+    #     self.c2 = c2
+    #     y2 = self.opt_firm(p1)[1]
+    #     # Compute U
+    #     U = np.log(c1**self.alpha * c2**(1-self.alpha)) - self.nu*l**(1+self.epsilon)/(1+self.epsilon)
 
-        SWF = U-self.kappa*y2
+    #     SWF = U-self.kappa*y2
         
-        return - SWF
+    #     return - SWF
 
-    def eq_constraint(self, input):
-            tau, T = input
-            return tau-T*self.c2 
+    # def eq_constraint(self, input):
+    #         tau, T = input
+    #         return tau-T*self.c2 
 
 
 
-    def SWF_solve(self):
+    # def SWF_solve(self):
 
-        l_quess = (0,0)
+    #     l_quess = (0,0)
 
-        eq_con = ({'type': 'eq', 'fun': self.eq_constraint})
+    #     eq_con = ({'type': 'eq', 'fun': self.eq_constraint})
 
-        bud1_con = {'type':'ineq','fun':lambda x: self.p1}
-        bud2_con = {'type':'ineq','fun':lambda x: self.p2}
-        bud3_con = {'type':'ineq','fun':lambda x: x[0]}
-        bud4_con = {'type':'ineq','fun':lambda x: x[1]}
+    #     bud1_con = {'type':'ineq','fun':lambda x: self.p1}
+    #     bud2_con = {'type':'ineq','fun':lambda x: self.p2}
+    #     bud3_con = {'type':'ineq','fun':lambda x: x[0]}
+    #     bud4_con = {'type':'ineq','fun':lambda x: x[1]}
 
-        sol = optimize.minimize(self.SWF_obj, l_quess,method='SLSQP', constraints = (eq_con,bud1_con, bud2_con, bud3_con, bud4_con)) 
-        return sol
+    #     sol = optimize.minimize(self.SWF_obj, l_quess,method='SLSQP', constraints = (eq_con,bud1_con, bud2_con, bud3_con, bud4_con)) 
+    #     return sol
 
 
 
